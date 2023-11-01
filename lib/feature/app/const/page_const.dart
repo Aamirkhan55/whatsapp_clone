7b8e6fe1f6a7c5eb05160246b904957413507df0
Amir55143 <aamirnoor55143@gmail.com>
2023-11-01 15:20:05 +0500

    Page-const

---
+++ lib/feature/app/const/page_const.dart
@@ -0,0 +1,26 @@
+class PageConst {
+  //Auth Const
+  static const String wellcomePage = "/";
+  static const String otpPage = "otpPage";
+  static const String loginPage = "loginPage";
+  static const String initialProfileSubmitPage = "initialProfileSubmitPage";
+
+  //Home
+  static const String homePage = "homePage";
+
+  //Contact User
+  static const String contactUserPage = "contactUserPage";
+  static const String callContactPage = "callContactPage";
+
+  //Setting
+  static const String settingPage = "settingPage";
+
+  //Chat
+  static const String singleChatPage = "chatPage";
+
+  //Status
+  static const String myStatusPage = "statusPage";
+
+  // Call
+  static const String callPage = "callPage";
+}
