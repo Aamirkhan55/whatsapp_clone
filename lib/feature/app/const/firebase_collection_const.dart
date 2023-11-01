5d33f20bc1a488200629378c788b30f92adc3899
Amir55143 <aamirnoor55143@gmail.com>
2023-11-01 10:42:43 +0500

    Firebase Const

---
+++ lib/feature/app/const/firebase_collection_const.dart
@@ -0,0 +1,20 @@
+class FirebaseCollectionConst {
+
+  //Users
+  static const String users = 'users';
+
+  //Chats
+  static const String oneToOneChannelChat = 'onToOneChannelChat';
+  static const String chatChannel = 'chatChannel';
+  static const String myChat = 'myChat';
+  static const String message = 'message';
+
+  //Status
+  static const String status = 'status';
+
+  //Calls
+  static const String call = 'call';
+  static const String callHistory = 'callHistory';
+
+
+}
