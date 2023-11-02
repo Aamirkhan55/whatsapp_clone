3cd4171f501b1761309483eccca157bf92134086
Amir55143 <aamirnoor55143@gmail.com>
2023-11-02 10:36:36 +0500

    Splash/Assets

--- lib/feature/app/splash/splash_page.dart
+++ lib/feature/app/splash/splash_page.dart
@@ -1,24 +1,72 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/feature/app/theme/style.dart';
+import 'package:whatsapp_clone/feature/presentation/pages/wellcome_page.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 5), () { 
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const WellcomePage() ));
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(
-       title: const Text('Splash Screen'),
-       backgroundColor: appBarColor,
-      ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(Icons.call),
-          Align(
-            alignment: Alignment.center,
-            child:Text('Whatsapp'),
-          ),
+          Container(),
+          Image.asset(
+            "assets/whats_app_logo.png",
+            color: Colors.white,
+            width: 100,
+            height: 100,
+            ),
+          Column(
+            children: [
+              const Text(
+                "from",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.green,
+                ),
+                ),
+              const SizedBox(height: 10),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/meta.png',
+                    color: Colors.white,
+                    width: 35,
+                    height: 35,
+                    ),
+                const SizedBox(width: 5),
+                const Text(
+                  'Meta',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  ),
+
+                ],
+               ), 
+            ],
+          )  
         ],
       ),
     );
