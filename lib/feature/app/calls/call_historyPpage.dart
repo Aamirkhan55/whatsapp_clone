1eb1cd5d534da014556d32caf109de37eb53f78c
Amir55143 <aamirnoor55143@gmail.com>
2023-11-03 12:45:57 +0500

    CallPage

--- lib/feature/app/calls/call_historyPpage.dart
+++ lib/feature/app/calls/call_historyPpage.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/feature/app/theme/style.dart';
 
 class CallHistoryPage extends StatelessWidget {
   const CallHistoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: Text("CallHistoryPage"),
+      ),
+    );
   }
 }
