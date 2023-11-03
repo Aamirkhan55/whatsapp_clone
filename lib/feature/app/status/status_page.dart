53b27328a8beb923fa5158c2b36d43744872fbfa
Amir55143 <aamirnoor55143@gmail.com>
2023-11-03 12:44:43 +0500

    StatusPage

--- lib/feature/app/status/status_page.dart
+++ lib/feature/app/status/status_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/feature/app/theme/style.dart';
 
-class ChatPage extends StatelessWidget {
-  const ChatPage({super.key});
+class StatusPage extends StatelessWidget {
+  const StatusPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: backgroundColor,
       body: Center(
-        child: Text("ChatPage"),
+        child: Text("StatusPage"),
       ),
     );
   }
