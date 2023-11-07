08013a678312915c5886643495068fb34e9bdf72
Amir55143 <aamirnoor55143@gmail.com>
2023-11-07 12:11:09 +0500

    Routes

---
+++ lib/routes/on_generate.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/feature/app/const/page_const.dart';
+import 'package:whatsapp_clone/feature/app/home/contact_page.dart';
+import 'package:whatsapp_clone/feature/app/setting/setting_page.dart';
+import 'package:whatsapp_clone/feature/calls/presentation/pages/call_contact_page.dart';
+import 'package:whatsapp_clone/feature/chat/presentation/pages/single_chat_page.dart';
+import 'package:whatsapp_clone/feature/status/presentation/pages/my_status.dart';
+
+
+class OnGenerateRoute {
+
+
+  static Route<dynamic>? route(RouteSettings settings) {
+    final args = settings.arguments;
+    final name = settings.name;
+
+    switch (name) {
+      case PageConst.contactUserPage:
+        {
+          return materialPageBuilder(const ContactsPage());
+        }
+      case PageConst.settingPage: {
+        return materialPageBuilder(const SettingPage());
+      }
+      case PageConst.callContactPage: {
+        return materialPageBuilder(const CallContactsPage());
+
+      }
+      case PageConst.myStatusPage: {
+        return materialPageBuilder(const MyStatusPage());
+      }
+      case PageConst.singleChatPage: {
+        return materialPageBuilder(const SingleChatPage());
+
+      }
+    }
+    return null;
+
+
+   }
+
+  }
+
+dynamic materialPageBuilder(Widget child) {
+  return MaterialPageRoute(builder: (context) => child);
+}
