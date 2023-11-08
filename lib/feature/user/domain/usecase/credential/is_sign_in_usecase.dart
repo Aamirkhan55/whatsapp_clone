8eec66338789b13093de5b7880e7519d9afff075
Amir55143 <aamirnoor55143@gmail.com>
2023-11-08 15:09:14 +0500

    SignIn_Usecase

---
+++ lib/feature/user/domain/usecase/credential/is_sign_in_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:whatsapp_clone/feature/user/domain/repository/user_reprository.dart';
+
+class IsSignInUseCase {
+  final UserRepository repository;
+
+  IsSignInUseCase({required this.repository});
+
+  Future<bool> call() async {
+    return repository.isSignIn();
+  }
+
+}
